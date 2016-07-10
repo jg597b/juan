a35f4a4a7365d24d0c50f6ab8d821f62a691d0ab
Juan Pablo Gonzalez <robin2243@hotmail.com>
2016-07-10 11:11:35 -0700

    task 4  create a scope called recent that takes an argument n (the number of albums) and returns only the last n albums released during the past 4 months.

--- app/models/album.rb
+++ app/models/album.rb
@@ -6,4 +6,5 @@ class Album < ActiveRecord::Base
   validates :cover_art, presence: true
   validates :released_on, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: "should be in the format YYYY-MM-DD" }
   scope :latest_released, -> {order('released_on DESC').first}
+  scope :recent, -> (n) { where(released_on: ((Date.today - 4.months)..Date.today)).last(n) }
 end
