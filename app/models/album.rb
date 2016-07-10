2217d6708eafd166dc741f0a3e7d46b4735608e2
Juan Pablo Gonzalez <robin2243@hotmail.com>
2016-07-10 11:51:47 -0700

    task 6 publisher is required when album saved

--- app/models/album.rb
+++ app/models/album.rb
@@ -5,6 +5,7 @@ class Album < ActiveRecord::Base
   validates :name, presence: true, length: { in: 2..50 }
   validates :cover_art, presence: true
   validates :released_on, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: "should be in the format YYYY-MM-DD" }
+  validates :publisher_id, presence:true
   scope :latest_released, -> {order('released_on DESC').first}
   scope :recent, -> (n) { where(released_on: ((Date.today - 4.months)..Date.today)).last(n) }
 end
